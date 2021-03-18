cb16fde8b0e46ac0d02e4cc4c1c766fa8c4702aa
student <student@ip-172-31-51-145.us-west-2.compute.internal>
2021-03-17 18:17:32 -0700

    03.17.2021

---
+++ src/_05_minion/Minion.java
@@ -0,0 +1,39 @@
+package _05_minion;
+
+public class Minion {
+	String name;
+	int eyes;
+	String color;
+	String empty;
+	Stirng master;
+	
+	
+	//Constructor
+	
+	Minion(String name, int eyes, String color, String empty)
+	this.name = name;
+	this.eyes = eyes;
+	this.color = color;
+	this.empty = empty;
+	]
+			public String getName() {
+				return name;
+			}
+			
+			public int getEyes() {
+				return eyes;
+				
+			}
+			public String getColor() {
+				return color;
+			}
+			public void setMaster(String masterName) {
+				master = masterName;
+			}
+			
+			public String getMaster() {
+				return master;
+			}
+
+
+}
